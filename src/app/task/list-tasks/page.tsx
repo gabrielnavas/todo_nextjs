d0989a600e6355a694df334aae66a4be7757a57e
navas <gabrielnavas@email.com>
2023-05-14 16:41:38 -0300

    remove spaces

--- src/app/task/list-tasks/page.tsx
+++ src/app/task/list-tasks/page.tsx
@@ -6,19 +6,13 @@ import TasksList from "./tasks-list"
 import TasksListHeader from "./tasks-list-header"
 
 
-
-const ListTasks = () => {
-  
- 
-  
-  return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <TasksListHeader />
-        <TasksList />
-      </div>
+const ListTasks = () => (
+  <div className={styles.container}>
+    <div className={styles.card}>
+      <TasksListHeader />
+      <TasksList />
     </div>
-  )
-}
+  </div>
+)
 
 export default ListTasks
